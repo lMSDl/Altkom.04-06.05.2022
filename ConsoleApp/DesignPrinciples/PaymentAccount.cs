2767e2e855b19c78b01cec34f7377e464b54ed12
Paweł Łęczycki <pawel@msd.info.pl>
2022-05-04 10:00:35 +0200

    KISS

---
+++ ConsoleApp/DesignPrinciples/PaymentAccount.cs
@@ -0,0 +1,22 @@
+﻿namespace DesignPrinciples
+{
+    public class PaymentAccount
+    {
+        public PaymentAccount(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get; }
+        public string CustomerName { get; set; }
+        public float Income { get; set; }
+        public float Outcome { get; set; }
+        public float AllowedDebit { get; set; }
+
+        public bool HasDebit()
+        {
+            return Income - Outcome < 0;
+        }
+    }
+}
+}
---
+++ ConsoleApp/DesignPrinciples/PaymentService.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DesignPrinciples
+{
+    public class PaymentService
+    {
+        private ICollection<PaymentAccount> PaymentAccounts { get; } = new List<PaymentAccount> { new PaymentAccount(1), new PaymentAccount(2), new PaymentAccount(3), new PaymentAccount(4), new PaymentAccount(5) };
+
+        public bool DeletePaymentAccount(PaymentAccount paymentAccount)
+        {
+            return PaymentAccounts.Remove(paymentAccount);
+        }
+
+        public PaymentAccount FindByAllowedDebig(float allowedDebit)
+        {
+            return PaymentAccounts.SingleOrDefault(x => x.AllowedDebit == allowedDebit);
+        }
+
+        public bool Charge(int id, float amount)
+        {
+            var paumentAccount = PaymentAccounts.SingleOrDefault(x => x.Id == id);
+            if (paumentAccount == null)
+            {
+                return false;
+            }
+
+            if (paumentAccount.Income - paumentAccount.Outcome + paumentAccount.AllowedDebit < amount)
+            {
+                return false;
+            }
+
+            paumentAccount.Outcome += amount;
+            return true;
+        }
+
+        public void Fund(int id, float amount)
+        {
+            var paumentAccount = PaymentAccounts.Where(x => x.Id == id).SingleOrDefault();
+            if (paumentAccount == null)
+            {
+                return;
+            }
+
+            paumentAccount.Income += amount;
+        }
+
+        public float? GetBalance(int id)
+        {
+            var paymentAccount = PaymentAccounts.Where(x => x.Id == id).SingleOrDefault();
+            return paymentAccount?.Income - paymentAccount?.Outcome;
+        }
+    }
+}

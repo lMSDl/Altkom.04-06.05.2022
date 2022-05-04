c7a62292843e9282857ee72b9c5de4e989fabb89
Paweł Łęczycki <pawel@msd.info.pl>
2022-05-04 10:30:00 +0200

    SOC

---
+++ ConsoleApp/DesignPrinciples/Customer.cs
@@ -0,0 +1,15 @@
+﻿namespace DesignPrinciples
+{
+    public class Customer
+    {
+        public Customer(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get; }
+        public string CustomerName { get; set; }
+        public PaymentAccount PaymentAccount { get; } = new PaymentAccount();
+    }
+}
+}
--- ConsoleApp/DesignPrinciples/PaymentService.cs
+++ ConsoleApp/DesignPrinciples/PaymentService.cs
@@ -5,23 +5,14 @@ namespace DesignPrinciples
 {
     public class PaymentService
     {
-        private ICollection<PaymentAccount> PaymentAccounts { get; } = new List<PaymentAccount> { new PaymentAccount(1), new PaymentAccount(2), new PaymentAccount(3), new PaymentAccount(4), new PaymentAccount(5) };
-
-        public bool Charge(int id, float amount)
+        public bool Charge(Customer customer, float amount)
         {
-            var paymentAccount = FindById(id);
-            return paymentAccount?.Charge(amount) ?? false;
+            return customer?.PaymentAccount.Charge(amount) ?? false;
         }
 
-        public void Fund(int id, float amount)
+        public void Fund(Customer customer, float amount)
         {
-            var paymentAccount = FindById(id);
-            paymentAccount?.Fund(amount);
-        }
-
-        private PaymentAccount FindById(int id)
-        {
-            return PaymentAccounts.SingleOrDefault(x => x.Id == id);
+            customer?.PaymentAccount.Fund(amount);
         }
     }
 }

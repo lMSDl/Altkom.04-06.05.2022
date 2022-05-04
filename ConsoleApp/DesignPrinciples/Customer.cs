077e01bf8260e4d96cbde48bf163b378f551667b
Paweł Łęczycki <pawel@msd.info.pl>
2022-05-04 11:57:36 +0200

    LSP

--- ConsoleApp/DesignPrinciples/Customer.cs
+++ ConsoleApp/DesignPrinciples/Customer.cs
@@ -12,4 +12,4 @@
         public PaymentAccount PaymentAccount { get; } = new PaymentAccount();
     }
 }
-}
+
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using DesignPrinciples;
+﻿using ConsoleApp.SOLID.L;
+using DesignPrinciples;
 using System;
 
 namespace ConsoleApp
@@ -7,13 +8,36 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            Rectangle shape = new Square();
+
+            var a = 4;
+            var b = 6;
+
+            Fill(shape, a, b);
+
+            Console.WriteLine($"{a}*{b}=");
+            ShowArea(shape);
+
+        }
+
+        static void Fill(Rectangle rectangle, int a, int b)
+        {
+            rectangle.A = a;
+            rectangle.B = b;
+        }
+
+        static void ShowArea(Rectangle rectangle)
+        {
+            Console.WriteLine(rectangle.Area);
+        }
+
+        private static void DesignPrinciples()
+        {
             var customersService = new CustomersService();
             var paymentService = new PaymentService();
 
             var customer = customersService.FindById(1);
             paymentService.Charge(customer.PaymentAccount, 100);
-
-
         }
     }
 }

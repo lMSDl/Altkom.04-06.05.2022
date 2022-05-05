fd11bccd758c24a75a6d0723cb67beaba58ea0e1
Paweł Łęczycki <pawel@msd.info.pl>
2022-05-05 14:49:45 +0200

    PrivateClassData

---
+++ ConsoleApp/DesignPatterns/Structural/PrivateClassData/SomeClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.DesignPatterns.Structural.PrivateClassData
+{
+    public class SomeClass
+    {
+
+        //private int _int;
+        //private string _string;
+        private SomeClassData _data;
+
+        public SomeClass(int @int, string @string)
+        {
+            _data = new SomeClassData(@int, @string);
+        }
+
+
+        public void SomeMethod()
+        {
+            //_int = 0;
+            //_string = "";
+            
+        }
+    }
+}
---
+++ ConsoleApp/DesignPatterns/Structural/PrivateClassData/SomeClassData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.DesignPatterns.Structural.PrivateClassData
+{
+    public class SomeClassData
+    {
+        private int _int;
+        private string _string;
+
+        public SomeClassData(int @int, string @string)
+        {
+            _int = @int;
+            _string = @string;
+        }
+
+        public int GetInt()
+        {
+            return _int;
+        }
+
+        public string GetString()
+        {
+            return _string;
+        }
+    }
+}
